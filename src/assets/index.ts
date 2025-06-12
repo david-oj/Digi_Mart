96cacc436d178eb0a8c135d57842641e29350ab4
David-A-Ojo <davidojo.dev@gmail.com>
2025-06-12 16:15:12 +0100

    chore(images): optimize images to webp format

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,23 +1,23 @@
-import fashionImg1 from "./fashionImg1.jpg";
-import electronics from "./electronics.jpeg";
-import fashionImg3 from "./fashionImg3.jpg";
-import home_decor from "./home_decor.jpg";
-import beauty from "./beauty.jpg";
-import fragrance from "./fragrance.jpeg";
-import headphone from "./headphone.jpeg";
-import smartwatch from "./smartwatch.jpg";
-import gifts from "./gifts.jpg";
-import appleStore from "./appleStore.png";
-import googleStore from "./googleStore.png";
-import customer1 from "./customer1.jpg"
-import customer2 from "./customer2.jpg"
-import customer3 from "./customer3.jpg"
+import fashionImg1 from "./fashionImg1.webp";
+import electronics from "./electronics.webp";
+import fashionImg3 from "./fashionImg3.webp";
+import home_decor from "./home_decor.webp";
+import beauty from "./beauty.webp";
+import fragrance from "./fragrance.webp";
+import headphone from "./headphone.webp";
+import smartwatch from "./smartwatch.webp";
+import gifts from "./gifts.webp";
+import appleStore from "./appleStore.webp";
+import googleStore from "./googleStore.webp";
+import customer1 from "./customer1.webp"
+import customer2 from "./customer2.webp"
+import customer3 from "./customer3.webp"
 
 export {
   fashionImg1,
   electronics,
   fashionImg3,
-  home_decor,
+  home_decor, 
   beauty,
   fragrance,
   headphone,
